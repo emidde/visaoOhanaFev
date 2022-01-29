bcf8aa7f0513dc8e5483630ec9cd23ca8bf0f25d
emidde <75273014+emidde@users.noreply.github.com>
2022-01-29 16:16:06 -0300

    Retirada do zoom ao tocar no formulário

--- pages/componentes/IndexPage.js
+++ pages/componentes/IndexPage.js
@@ -5,7 +5,7 @@ function IndexPage() {
     <div>
       <Head>
         <title>Ohana</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width, maximum-scale=1.0, user-scalable=no" />
       </Head>
     </div>
   )

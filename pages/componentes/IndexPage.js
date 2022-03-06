603bde2a5583e701a28e7298ff8674c606c694cc
emidde <75273014+emidde@users.noreply.github.com>
2022-03-06 09:59:32 -0300

    Update IndexPage.js

--- pages/componentes/IndexPage.js
+++ pages/componentes/IndexPage.js
@@ -4,8 +4,8 @@ function IndexPage() {
   return (
     <div>
       <Head>
-        <title>Formulário Ohana</title>
-         <meta name="description" content="Formulario de inscricao para o evento Ohana no dia 12/02/2021" />
+        <title>Formulário Visão</title>
+        <meta name="description" content="Formulario de inscrição para o evento Ohana no dia 12/02/2021" />
         <meta name="author" content="Creative vision" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width, maximum-scale=1.0, user-scalable=no" />
       </Head>

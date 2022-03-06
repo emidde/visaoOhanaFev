41d8dbd4327b4c8669735749f35bc9964f3a7e9e
emidde <75273014+emidde@users.noreply.github.com>
2022-03-06 10:15:39 -0300

    Update IndexPage.js

--- pages/componentes/IndexPage.js
+++ pages/componentes/IndexPage.js
@@ -5,7 +5,7 @@ function IndexPage() {
     <div>
       <Head>
         <title>Formulário Visão</title>
-        <meta name="description" content="Formulario de inscrição para o evento Ohana no dia 12/02/2021" />
+        <meta name="description" content="Formulario de inscrição para o evento Visao Verso" />
         <meta name="author" content="Creative vision" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width, maximum-scale=1.0, user-scalable=no" />
       </Head>

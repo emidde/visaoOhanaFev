5a444c5fa8f7933b911615ed1e701445202ad6c3
emidde <75273014+emidde@users.noreply.github.com>
2022-03-06 09:59:53 -0300

    Update Header.js

--- pages/componentes/Header.js
+++ pages/componentes/Header.js
@@ -1,16 +1,16 @@
-import Head from 'next/head'
+import React from 'react';
+import Image from 'next/image';
+import logovisao from '../img/logotipo.png';
 
-function IndexPage() {
-  return (
-    <div>
-      <Head>
-        <title>Formulário Visão</title>
-        <meta name="description" content="Formulario de inscrição para o evento Ohana no dia 12/02/2021" />
-        <meta name="author" content="Creative vision" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width, maximum-scale=1.0, user-scalable=no" />
-      </Head>
-    </div>
-  )
-}
 
-export default IndexPage;;
+const Header = () => {
+    return (
+        <Image
+        src={logovisao}
+        height={80}
+        width={190}
+        />
+    );
+};
+
+export default Header;
